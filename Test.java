e506525aac0d7d607de68d884c1a833bbbb783b6
Anant Soni <61542253+AnantSoni-sudo@users.noreply.github.com>
2025-07-02 12:35:00 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+package first_project;
+
+public class Test {
+
+	// write a code to add to number
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int sum = a + b;
+		System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+	}
+	
+}
